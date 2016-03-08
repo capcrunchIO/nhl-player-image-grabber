39ebad8899a888c8841f8406f69294e502c52e4a
capcrunchIO <pucksdeep@capcrunch.io>
2016-03-08 13:19:57 -0800

    fixes logic which scans next row after initial default image found (Closes #2)

--- app.js
+++ app.js
@@ -26,39 +26,43 @@ function start() {
   horseman
   .open('https://www.nhl.com/player')
   .type('#searchTerm', name)
-  .wait(1000)
+  .wait(2000)
   .evaluate(function(fname, lname, err_sel, res_sel) {
-    var url, src = [0, '', 0, fname + ' ' + lname];
+    var res = [0, '', ''];
     // no results found
-    if ($(err_sel).css('display') === 'block') { src[0] = 'NOT_FOUND'; }
+    if ($(err_sel).css('display') === 'block') { res[0] = 'NOT_FOUND'; }
     // results found
     else if ($(res_sel).css('display') === 'table') {
-      var link_rx = new RegExp(fname.replace(/'/g, '') + '-' + lname.replace(/'/g, '') + '-', 'g');
-      var src_rx = new RegExp('(skater|goalie)', 'g');
+      var player_name = fname.replace(/'/g, '') + '-' + lname.replace(/'/g, '') + '-',
+          link_rx = new RegExp(player_name, 'g');
       // scan results
-      $('.results-table tbody tr td a').each(function(i) {
-        url = $(this).attr('href');
+      $('.results-table tbody tr').each(function(i) {
+        var url = $(this).find('td > a').attr('href'),
+            src = $(this).find('td > a > img').attr('src').replace('.jpg', '.png');
         // find image url
         if (link_rx.test(url)) {
-          src = ['IMG_FOUND', $('.results-table tbody tr td a:eq(' + i + ') img').attr('src').replace('.jpg', '.png'), url];
           // default image found
-          if (src && src_rx.test(src)) {
-            // next player has identical name + image found
-            url = $('.results-table tbody tr:eq(' + (i + 1) + ') td a').attr('href');
-            if (url) {
-              src = ['IMG_FOUND', $('.results-table tbody tr td a:eq(' + (i + 1) + ') img').attr('src').replace('.jpg', '.png'), url];
-            } else { src[0] = 'DEFAULT_IMG'; }
-          }
+          if (/(skater|goalie)/.test(src)) {
+            // check next player row
+            var alt_url = $(this).next().find('td > a').attr('href'),
+                alt_src = $(this).next().find('td > a > img').attr('src').replace('.jpg', '.png');
+                alt_rx = new RegExp(player_name, 'g');
+            if (alt_rx.test(alt_url) && !/(skater|goalie)/.test(alt_src)) {
+              res = ['IMG_FOUND', alt_src, alt_url, '*'];
+            } else { res = ['DEFAULT_IMG', src, url]; }
+          } else { res = ['IMG_FOUND', src, url]; }
           return false;
         }
       });
     }
-    return src;
+    return res;
   }, fname, lname, err_sel, res_sel)
   .then(function(res) {
     var chain = horseman;
-    if (res[0] === 'NOT_FOUND') { console.log('NOT_FOUND'); } // no results found
-    else if (res[0] === 'DEFAULT_IMG') { // scan alt player page
+    // no results found
+    if (res[0] === 'NOT_FOUND') { console.log('NOT_FOUND'); }
+    // scan alt player page
+    else if (res[0] === 'DEFAULT_IMG') {
       chain = chain
       .open('https://www.nhl.com' + res[2])
       .wait(1000)
@@ -69,7 +73,10 @@ function start() {
         return src;
       })
       .then(function(src) { console.log(src); })
-    } else if (res[0] === 'IMG_FOUND') { console.log(res[1]); } // image found
+    } else if (res[0] === 'IMG_FOUND') {
+      if (res[3] === '*') { console.log(res[3] + ' ' + res[1]); }
+      else { console.log(res[1]); }
+    }
     else { console.log('CONNECTION_ERROR'); }
     return chain;
   })
